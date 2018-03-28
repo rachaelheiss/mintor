8b2d154c3c4e430658057e55203105a1c7186a77
rachaelheiss <36305260+rachaelheiss@users.noreply.github.com>
2018-03-28 13:59:07 -0400

    Update LabelPhases.py
    Include regex and write out to csvs

--- LabelPhases.py
+++ LabelPhases.py
@@ -4,7 +4,7 @@ import os
 import re
 from sklearn.model_selection import train_test_split
 
-def data_read (read,write):
+def dataRead (read,write):
     with open(read) as datafile:
         data = csv.reader(datafile,delimiter = ',')
         for row in data:
@@ -12,13 +12,13 @@ def data_read (read,write):
                 write[x].append(float(row[x]))
     return (write)
 
-def data_write (datafile,datalist):
+def dataWrite (datafile,datalist):
     with open(datafile, 'wb') as csvfile:
         write = csv.writer(csvfile, delimiter = ',')
         for _ in datalist:
-            write.writerow([_])
+            write.writerow(_)
 
-def label_phases (t1,t2,t3,dataset):
+def labelPhases (ID,t1,t2,t3,dataset):
     #find index that matches timestamp of beginning of phase 1
     dataset = np.array(dataset).T.tolist()
     t = int(0)
@@ -30,73 +30,154 @@ def label_phases (t1,t2,t3,dataset):
             dataset[_].insert(4,int(1))
         elif all([dataset[_][0] >= t2, dataset[_][0] < t3]):
             dataset[_].insert(4,int(2))
-        elif all([dataset[_][0] >= t2, dataset[_][0] < t3]):
+        elif dataset[_][0] >= t3:
             dataset[_].insert(4,int(3))
+        dataset[_].insert(5,ID)
     return dataset
 
+def readAllForTrial(acl,acr,gyl,gyr):
+    lefta = [[],[],[],[]]
+    righta = [[],[],[],[]]
+    leftg = [[],[],[],[]]
+    rightg = [[],[],[],[]]
+
+    lefta = dataRead(acl,lefta)
+    righta = dataRead(acr,righta)
+    leftg = dataRead(gyl,leftg)
+    rightg = dataRead(gyr,rightg)
+    return(lefta, righta, leftg, rightg)
+
+def processByFile(gID,sID,tID,leftacc,rightacc,leftgyro,rightgyro):
+    print leftacc
+
+    l_a,r_a,l_g,r_g = readAllForTrial(leftacc,rightacc,leftgyro,rightgyro)
+
+    # Create threshold for pick up blade
+    threshold = np.mean(abs(np.asarray((l_a[3][:25]))))*1
+
+    # Compare z-movement against threshold, set = "start"
+    index = 0
+    while abs(l_a[3][index]) < threshold:
+        index += 1
+    p1t = l_a[0][index]
+    p1i = index
+    print p1i
+    print threshold
+    print p1t
+
+    # Find location where motion begins to go steady again
+    while abs(max(l_a[3][index:index+100]) - min(l_a[3][index:index+100])) > 0.15:
+        index += 1
+    p2t = l_a[0][index]
+    p2i = index
+
+    # Create threshold for take blade out, compare against threshold
+    threshold = abs(max(l_a[3][index:index+50]))
+    while abs(l_a[3][index]) < threshold:
+        index += 1
+        '''
+        print index
+        print l_a[3][index]
+        print threshold
+        '''
+    p3t = l_a[0][index]
+    p3i = index
+
+    l_a = labelPhases(gID,p1t,p2t,p3t,l_a)
+    # r_a = labelPhases(gID,p1t,p2t,p3t,r_a)
+    l_g = labelPhases(gID,p1t,p2t,p3t,l_g)
+    # r_g = labelPhases(gID,p1t,p2t,p3t,r_g)
+
+    # l_a = labelPhases(gID,0,0,0,l_a)
+    # r_a = labelPhases(gID,0,0,0,r_a)
+    # l_g = labelPhases(gID,0,0,0,l_g)
+    # r_g = labelPhases(gID,0,0,0,r_g)
+
+    dataWrite(str(sID) + "-" + str(tID) + "acc-left-labeled.csv",l_a)
+    dataWrite(str(sID) + "-" + str(tID) + "gyro-left-labeled.csv",l_g)
+    dataWrite(str(sID) + "-" + str(tID) + "acc-right-labeled.csv",r_a)
+    dataWrite(str(sID) + "-" + str(tID) + "gyro-right-labeled.csv",r_g)
+
+
+'''
+'''
+
+train_data = []
 filelist = []
-splitlist = []
+test_data = []
 
-#find only .csv files and exclude problem data and first trials
-path = "./"
-extension = ".csv"
-for root, dirs, files in os.walk(path):
-    for name in files:
-        if os.path.splitext(name)[-1] == extension:
-            if (re.search(r'Problem Data',root) == None) and (re.search(r'Slow',root) == None) and (re.search(r'[-_\w]+1-left.csv$',name) == None) and (re.search(r'[-_\w]+1-right.csv$', name) == None):
-                filelist.append(os.path.join(root,name))
-                if re.search(r'^Acc[-_\w]+left\.csv$',name):
-                    splitlist.append(os.path.join(root,name))
+with open('training_set.csv') as trainfile:
+    data = csv.reader(trainfile,delimiter=',')
+    for _ in data:
+        train_data.append(str(_))
 
-# Proportion of trials for test
-test_proportion = .3
+with open('test_set.csv') as testfile:
+    data = csv.reader(testfile,delimiter=',')
+    for _ in data:
+        test_data.append(str(_))
 
-train_list,test_list = train_test_split(splitlist,test_size = test_proportion)
-print ('Training length: ' + str(len(train_list)))
-print ('Test length: ' + str(len(test_list)))
-print ('Total length: ' + str(len(splitlist)))
+with open('file_set.csv') as filedata:
+    data = csv.reader(filedata,delimiter=',')
+    for _ in data:
+        filelist.append(str(_))
 
-# Write to .csv file: training data and test data
-data_write('training_set.csv', train_list)
-data_write('test_set.csv', test_list)
+for _ in train_data:
+    trialID = _[-12:-11]
+    AR = str()
+    AL = str()
+    GR = str()
+    GL = str()
+    if (re.search(r'CA1',_)):
+        groupID = 0
+        subjID = "./CA1-2016/" + str(_[13:15]) + "/"
+    elif(re.search(r'Attendings',_)):
+        groupID = 1
+        subjID = "./Attendings/" + str(_[15:17]) + "/"
+    for f in filelist:
+        f = f[2:-2]
+        regex = re.escape(subjID) + r'[\-\_\/0-9A-Za-z]*' + re.escape(trialID) + r"\-(left|right)\.csv$"
+        # re.escape(subjID) + r'[\-\_\/0-9A-Za-z]*' + re.escape(trialID) + r"\-left\.csv$"
+        if (re.search(regex,f) != None):
+            if (re.search(r'Acc', f) != None):
+                if (re.search(r'left', f) != None):
+                    AL = f
+                else:
+                    AR = f
+            elif (re.search(r'Gyro',f) != None):
+                if (re.search(r'left', f) != None):
+                    GL = f
+                else:
+                    GR = f
 
-accL = [[], [], [], []]
-gyroL = [[], [], [], []]
-gyroR = [[], [], [], []]
-accR = [[], [], [], []]
+    processByFile(groupID,subjID,trialID,AL,AR,GL,GR)
 
-accL = data_read('Attendings/1/Sim/Acc-1-left.csv',accL)
-accR = data_read('Attendings/1/Sim/Acc-1-right.csv',accR)
-gyroL = data_read('Attendings/1/Sim/Gyro-1-left.csv',gyroL)
-gyroR = data_read('Attendings/1/Sim/Gyro-1-right.csv',gyroR)
+    #feed file info into readallfortrial
+    #process each file w processbyfile
 
-# Create threshold for pick up blade
-threshold = (max(accL[3][:50]))*1.1
-
-# Compare z-movement against threshold, set = "start"
-index = 0
-while accL[3][index] < threshold:
-    index += 1
-p1t = accL[0][index]
-p1i = index
-
-# Find location where motion begins to go steady again
-while abs(max(accL[3][index:index+25]) - min(accL[3][index:index+25])) > 0.1:
-    index += 1
-p2t = accL[0][index]
-p2i = index
-
-# Create threshold for take blade out, compare against threshold
-threshold = max(accL[3][index:index+25])*1.1
-while accL[3][index] < threshold:
-    index += 1
-p3t = accL[0][index]
-p3i = index
-
-accL = label_phases(p1t,p2t,p3t,accL)
-accR = label_phases(p1t,p2t,p3t,accR)
-gyroL = label_phases(p1t,p2t,p3t,gyroL)
-gyroR = label_phases(p1t,p2t,p3t,gyroR)
-
-print('AccL: ' + str(accL[0:2]))
-print('GyroL: ' + str(gyroL[0:2]))
+for _ in test_data:
+    trialID = _[-12:-11]
+    AR = str()
+    AL = str()
+    GR = str()
+    GL = str()
+    if (re.search(r'CA1',_)):
+        groupID = 0
+        subjID = "./CA1-2016/" + str(_[13:15]) + "/"
+    elif(re.search(r'Attendings',_)):
+        groupID = 1
+        subjID = "./Attendings/" + str(_[15:17]) + "/"
+    for f in filelist:
+        f = f[2:-2]
+        regex = re.escape(subjID) + r'[\-\_\/0-9A-Za-z]*' + re.escape(trialID) + r"\-(left|right)\.csv$"
+        if (re.search(regex,f) != None):
+            if (re.search(r'Acc', f) != None):
+                if (re.search(r'left', f) != None):
+                    AL = f
+                else:
+                    AR = f
+            elif (re.search(r'Gyro',f) != None):
+                if (re.search(r'left', f) != None):
+                    GL = f
+                else:
+                    GR = f
+    processByFile(groupID,subjID,trialID,AL,AR,GL,GR)
